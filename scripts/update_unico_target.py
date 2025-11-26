a1512761d56472fce4c995e2ee97256de08d9a99
Kaique Pinheiro <kaique.pinheiro@unico.io>
2025-11-26 10:02:28 -0300

    Fix: correct auto-update script endpoint
    # Description
    
    Update source URL to fix automatic updates
    
    # Demand
    
    What demand does this Pull Request refer to?
    
    - [ ] Bug
    - [ ] Feature
    - [X] update

--- scripts/update_unico_target.py
+++ scripts/update_unico_target.py
@@ -9,7 +9,7 @@ import time
 # ===============================
 # Settings
 # ===============================
-URL = "https://devcenter.unico.io/idcloud/integracao/sdk/integracao-sdks/sdk-ios/release-notes"
+URL = "https://devcenter.unico.io/unico-idcloud/by-client-integration/pt/sdk/sdks-disponiveis/sdk-ios/release-notes"
 DEPENDENCY = "unicocheck-ios"
 FILE_TO_UPDATE = "Podfile"
 
